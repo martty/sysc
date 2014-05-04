2a6d090520500d74b65af334630340e60ad924d9
Marcell Kiss <marcellk.kiss@gmail.com>
2014-05-04 18:49:07 +0200

    start

---
+++ sorhajokapitanymag.cpp
@@ -0,0 +1,45 @@
+#ifndef SORHAJO_BEHAVIORAL
+#define SORHAJO_BEHAVIORAL
+//-------------------------------------------------------------------------------------------------
+#include <systemc.h>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <map>
+//-------------------------------------------------------------------------------------------------
+struct SorhajoKapitany: public sc_module {
+  
+  // ports -----------------------------
+  sc_in   < bool >        Clk;
+  sc_in   < sc_lv<1> >    Reset;
+  // internal resources ----------------
+  sc_lv<8>      PC;     // instruction pointer
+  sc_lv<8>      P;		// zero flag
+
+  
+  //-----------------------------------------------------------------------------------------------
+  void FunctionThread() {
+    while (1) {
+      if (Reset.read() == "1") {
+        // reset all internal variables
+        PC = 0;
+        P = 0;
+      }
+      else {
+        
+      wait();
+    }
+  }
+  //-----------------------------------------------------------------------------------------------
+  // contructor ------------------------
+  SC_CTOR(DemoCore_Behavioral) {
+    SC_THREAD(FunctionThread); sensitive << Clk.pos();
+    // initialize internal variables ---
+    PC = 0;
+    P = 0;
+  }
+
+};
+//-------------------------------------------------------------------------------------------------
+#endif; /* SORHAJO_BEHAVIORAL */
+//-------------------------------------------------------------------------------------------------
