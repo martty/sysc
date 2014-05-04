9a70abf41db877b1a5530771373fd3f7bafc44dd
Marcell Kiss <marcellk.kiss@gmail.com>
2014-05-04 22:09:10 +0200

    lotsacode

--- sorhajokapitanymag.cpp
+++ sorhajokapitanymag.cpp
@@ -40,14 +40,14 @@
 	static const unsigned inst_rts_imp = 0x60;
 	static const unsigned inst_lda_imm = 0xa9;
 	static const unsigned inst_lda_abs = 0xad;
-	static const unsigned inst_lda_x = 0xa1;
+	static const unsigned inst_lda_abs_x = 0xbd;
 	static const unsigned inst_ldx_imm = 0xa2;
 	static const unsigned inst_ldx_abs = 0xae;
 	static const unsigned inst_ldy_imm = 0xa0;
 	static const unsigned inst_pla_imp = 0x68;
 	static const unsigned inst_pha_imp = 0x48;
 	static const unsigned inst_sta_abs = 0x8d;
-	static const unsigned inst_sta_x = 0x81;
+	static const unsigned inst_sta_abs_x = 0x9d;
 	static const unsigned inst_stx_abs = 0x8e;
 	static const unsigned inst_sty_abs = 0x8c;
 	static const unsigned inst_tax_imp = 0xaa;
@@ -61,7 +61,7 @@ int inst_type[41]={ inst_clc_imp, inst_dex_imp, inst_dey_imp, inst_inx_imp, inst
 					inst_bcc_rel, inst_beq_rel, inst_bne_rel, inst_bsc_rel,
 					inst_adc_abs, inst_asl_abs, inst_inc_abs, inst_jmp_abs, inst_jsr_abs, inst_lda_abs, inst_ldx_abs, inst_rol_abs, inst_sta_abs, inst_stx_abs,
 					inst_sty_abs,
-					inst_lda_x, inst_sta_x
+					inst_lda_abs_x, inst_sta_abs_x
 					};
 //	0-13: 1
 //	14-26: 2
@@ -156,6 +156,94 @@ struct SorhajoKapitany: public sc_module {
 				else
 					P.C = (t>255) ? 1:0;
 					A = t & 0xFF  ;
+				return;
+			
+			case inst_lda_imm:
+				A = op1;
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
+				return
+	
+			case inst_lda_abs:
+				A = RAM[op2<<8 + op1];
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
+				return
+			
+			case inst_lda_abs_x:
+				A = RAM[op2<<8 + op1 + X.to_uint()];
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
+				return;
+	
+			case inst_ldx_imm:
+				X = op1;
+				N = X[7].to_bool();
+				Z = (X.to_uint() == 0);
+				return;
+	
+			case inst_ldx_abs:
+				X = RAM[op2<<8 + op1];
+				N = X[7].to_bool();
+				Z = (X.to_uint() == 0);
+				return;
+	
+			case inst_ldy_imm:
+				Y = op1;
+				N = Y[7].to_bool();
+				Z = (Y.to_uint() == 0);
+				return;
+	
+			case inst_pla_imp:
+				SP++;
+				A = RAM[SP.to_uint()];
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
+				return;
+				
+			case inst_pha_imp:
+				RAM[SP] = A.to_uint();
+				SP = SP-1;
+				return;
+				
+			case inst_sta_abs:
+				RAM[op2<<8 + op1] = A.to_uint();
+				return;
+			
+			case inst_sta_abs_x:
+				RAM[op2<<8 + op1 + X.to_uint()] = A.to_uint();
+				return;
+			
+			case inst_stx_abs:
+				RAM[op2<<8 + op1] = X.to_uint();
+				return;
+
+			case inst_sty_abs:
+				RAM[op2<<8 + op1] = Y.to_uint();
+				return;
+			
+			case inst_tax_imp:
+				X = A;
+				N = X[7].to_bool();
+				Z = (X.to_uint() == 0);
+				return;
+			
+			case inst_tay_imp:
+				Y = A;
+				N = Y[7].to_bool();
+				Z = (Y.to_uint() == 0);
+				return;
+			
+			case inst_txa_imp:
+				A = X;
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
+				return;
+			
+			case inst_tya_imp:
+				A = Y;
+				N = A[7].to_bool();
+				Z = (A.to_uint() == 0);
 				return;
 		};
 	}
